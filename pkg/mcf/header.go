8504ddb741caf9188b7ce974c01ef35ff515088f
Sam Catlow <sam@catlow.tech>
2026-01-27 07:13:16 +1100

    move sections and heaers to their own files.

--- pkg/mcf/header.go
+++ pkg/mcf/header.go
@@ -2,6 +2,33 @@ package mcf
 
 import "unsafe"
 
+// MCF global constants must never change.
+const (
+	// MagicMCF is the file magic for all MCF containers.
+	// It is encoded as "MCF\0".
+	MagicMCF = "MCF\x00"
+
+	// Current Major Version: Any change indicates a breaking format change.
+	CurrentMajor uint16 = 1
+
+	// Current Minor Version: Versions may add new optional sections or fields.
+	CurrentMinor uint16 = 0
+
+	// Format level flags
+	FlagTensorDataAligned64 uint64 = 1 << 0
+)
+
+type MCFHeader struct {
+	Magic            [4]byte
+	Major            uint16
+	Minor            uint16
+	HeaderSize       uint32
+	SectionCount     uint32
+	SectionDirOffset uint64
+	FileSize         uint64
+	Flags            uint64
+}
+
 func (h *MCFHeader) Valid() bool {
 	if string(h.Magic[:]) != MagicMCF {
 		return false
--- pkg/mcf/section.go
+++ pkg/mcf/section.go
@@ -1,5 +1,21 @@
 package mcf
 
+type SectionType uint32
+
+const (
+	SectionModelInfo   SectionType = 0x0001
+	SectionQuantInfo   SectionType = 0x0002
+	SectionTensorIndex SectionType = 0x0003
+	SectionTensorData  SectionType = 0x0004
+)
+
+type MCFSection struct {
+	Type    uint32
+	Version uint32
+	Offset  uint64
+	Size    uint64
+}
+
 type Section struct {
 	MCFSection
 }
