6fdadec50a534a866cefde12f8fdbbcaba60ad46
Sam Catlow <sam@catlow.tech>
2026-01-27 07:12:25 +1100

    Add unsupported minor err

--- pkg/mcf/errors.go
+++ pkg/mcf/errors.go
@@ -5,5 +5,6 @@ import "errors"
 var (
 	ErrInvalidMagic     = errors.New("invalid MCF magic")
 	ErrUnsupportedMajor = errors.New("unsupported MCF major version")
+	ErrUnsupportedMinor = errors.New("unsupported MCF minor version")
 	ErrCorruptFile      = errors.New("corrupt MCF file")
 )
