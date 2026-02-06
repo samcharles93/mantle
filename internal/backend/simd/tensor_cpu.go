ce3b4102c8a7da13eae7fe28c8326527242ee5b2
Sam Catlow <sam@catlow.tech>
2026-02-06 22:58:25 +1100

    feat: expand CPUFeatures struct to include additional AVX capabilities

--- internal/backend/simd/tensor_cpu.go
+++ internal/backend/simd/tensor_cpu.go
@@ -4,11 +4,19 @@ import "simd/archsimd"
 
 // CPUFeatures holds detected CPU capabilities, checked once at init.
 type CPUFeatures struct {
-	HasAVX2 bool
+	HasAVX2       bool
+	HasFMA        bool
+	HasAVXVNNI    bool
+	HasAVX512     bool
+	HasAVX512VNNI bool
 }
 
 var cpu CPUFeatures
 
 func init() {
 	cpu.HasAVX2 = archsimd.X86.AVX2()
+	cpu.HasFMA = archsimd.X86.FMA()
+	cpu.HasAVXVNNI = archsimd.X86.AVXVNNI()
+	cpu.HasAVX512 = archsimd.X86.AVX512()
+	cpu.HasAVX512VNNI = archsimd.X86.AVX512VNNI()
 }
